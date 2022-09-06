6598d2d4895cad4a688a3654cf87bc50221e54c9
Dennis <dennisgachomo@gmail.com>
2022-09-06 18:13:09 +0300

    13th commit

--- script.js
+++ script.js
@@ -2,3 +2,11 @@ const from = document.getElementById("form");
 const search = document.getElementById("search");
 const result = document.getElementById("result");
 const more = document.getElementById("more");
+const apiURL = "https://api.lyrics.ovh";
+
+async function searchSongs(term) {
+  const res = await fetch(`${apiURL}/suggest/${term}`);
+  const data = await res.json();
+
+  showData(data);
+}
